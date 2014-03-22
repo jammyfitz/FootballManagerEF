649bd41d415444fc92b257a5f04efc066a039930
jammyfitz:tyrian2 <jammyfitz@hotmail.com>
2014-03-22 09:35:43 +0000

    First Check In for Football Manager - Entity Framework Edition

---
+++ FootballManagerEF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FootballManagerEF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ FootballManagerEF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FootballManagerEF
+{
+    /// *** Initial Thoughts to Class Hierarchy ***
+    /// 
+    /// A Team has many Players
+    /// A Match has many Players and a Player plays many Matches
+    /// A Game is a single Match
+    /// A Game has many Players
+    /// A Game has many Teams
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
