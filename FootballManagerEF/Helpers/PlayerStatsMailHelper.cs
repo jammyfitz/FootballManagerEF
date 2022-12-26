214f4d919a972bf38a65abb5fb7cad2fbe71f2ed
Jamie Fitzgerald <jammyfitz@hotmail.com>
2022-12-26 19:06:41 +0000

    2022 data backed up and Octopus v2.0 released

--- FootballManagerEF/Helpers/PlayerStatsMailHelper.cs
+++ FootballManagerEF/Helpers/PlayerStatsMailHelper.cs
@@ -35,16 +35,6 @@ namespace FootballManagerEF.Helpers
 
         public string GetBody()
         {
-            var body = new StringBuilder("***The Octopus v1.6***\n");
-
-            foreach (var playerStatistics in _playerStatisticsData)
-                body.AppendLine(WritePlayerStat(playerStatistics.PlayerName, playerStatistics.TotalMatchWins.ToString(), playerStatistics.WinRatio));
-
-            return body.ToString();
-        }
-
-        public string GetBody2023()
-        {
             var body = new StringBuilder();
             body.AppendLine("---------------------------------");
             body.AppendLine("   The Octopus v2.0              (.  .)  ");
@@ -53,24 +43,19 @@ namespace FootballManagerEF.Helpers
             body.AppendLine("Name - Wins - Win% - Average For(Total) - Average Against(Total)");
 
             foreach (var playerStatistics in _playerStatisticsData)
-                body.AppendLine(WritePlayerStat2(playerStatistics));
+                body.AppendLine(WritePlayerStat(playerStatistics));
 
             return body.ToString();
         }
 
-        private static string WritePlayerStat(string playerName, string matchWins, decimal winRatio)
-        {
-            return string.Format("{0} - {1} - {2}%", playerName, matchWins, winRatio.Round());
-        }
-
-        private static string WritePlayerStat2(PlayerStatisticsData playerStatisticsData)
+        private static string WritePlayerStat(PlayerStatisticsData playerStatisticsData)
         {
             var winRatio = playerStatisticsData.WinRatio.Round();
             var goalsFor = $"{playerStatisticsData.AverageGoalsForPerGame.Round()}({playerStatisticsData.GoalsFor})";
             var goalsAgainst = $"{playerStatisticsData.AverageGoalsAgainstPerGame.Round()}({playerStatisticsData.GoalsAgainst})";
 
             var playerStatsLine = $"{playerStatisticsData.PlayerName} - {playerStatisticsData.TotalMatchWins} - {winRatio}% - {goalsFor} - {goalsAgainst}";
-            
+
             return playerStatsLine;
         }
     }
