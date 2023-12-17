b5dcaa9e37de4ca517904ef1291cb22db81c469c
Jamie Fitzgerald <jammyfitz@hotmail.com>
2023-12-17 17:12:59 +0000

    Added functionality for annual stats reporting

--- FootballManagerEF/Helpers/PlayerStatsMailHelper.cs
+++ FootballManagerEF/Helpers/PlayerStatsMailHelper.cs
@@ -3,6 +3,7 @@ using FootballManagerEF.Interfaces;
 using FootballManagerEF.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FootballManagerEF.Helpers
@@ -45,14 +46,24 @@ namespace FootballManagerEF.Helpers
             foreach (var playerStatistics in _playerStatisticsData)
                 body.AppendLine(WritePlayerStat(playerStatistics));
 
+            ////AppendAnnualStatistics(body, _playerStatisticsData, 8);
+            
             return body.ToString();
+        }
+
+        private void AppendAnnualStatistics(StringBuilder body, List<PlayerStatisticsData> playerStatisticsData, int minimumMatchWinsForReport)
+        {
+            var playerStatsForAnnualReport = playerStatisticsData.Where(x => x.TotalMatchWins > minimumMatchWinsForReport);
+            body.AppendLine($"Season Winner: {playerStatsForAnnualReport.OrderByDescending(x => x.TotalMatchWins).First().PlayerName} ({playerStatsForAnnualReport.Max(x => x.TotalMatchWins)})");
+            body.AppendLine($"Best Striker: {playerStatsForAnnualReport.OrderByDescending(x => x.AverageGoalsForPerGame).First().PlayerName} ({playerStatsForAnnualReport.Max(x => x.AverageGoalsForPerGame)})");
+            body.AppendLine($"Best Defender: {playerStatsForAnnualReport.OrderBy(x => x.AverageGoalsAgainstPerGame).First().PlayerName} ({playerStatsForAnnualReport.Min(x => x.AverageGoalsAgainstPerGame)})");
         }
 
         private static string WritePlayerStat(PlayerStatisticsData playerStatisticsData)
         {
-            var winRatio = playerStatisticsData.WinRatio.Round();
-            var goalsFor = $"{playerStatisticsData.AverageGoalsForPerGame.Round()}({playerStatisticsData.GoalsFor})";
-            var goalsAgainst = $"{playerStatisticsData.AverageGoalsAgainstPerGame.Round()}({playerStatisticsData.GoalsAgainst})";
+            var winRatio = playerStatisticsData.WinRatio;
+            var goalsFor = $"{playerStatisticsData.AverageGoalsForPerGame}({playerStatisticsData.GoalsFor})";
+            var goalsAgainst = $"{playerStatisticsData.AverageGoalsAgainstPerGame}({playerStatisticsData.GoalsAgainst})";
 
             var playerStatsLine = $"{playerStatisticsData.PlayerName} - {playerStatisticsData.TotalMatchWins} - {winRatio}% - {goalsFor} - {goalsAgainst}";
 
